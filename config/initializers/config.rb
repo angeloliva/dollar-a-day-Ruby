f72965a64cb60bcd8727099f4c04afa2cb4d3f90
Dastrup Kennet <dastrup-star@Dastrups-Mac.local>
2017-10-18 14:04:28 +0200

    change config.rb

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,4 +1,4 @@
-CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'config.yml'))[Rails.env].symbolize_keys!
+
 
 
 # Load order issue -- gotta load these here instead of development.rb
@@ -10,7 +10,7 @@ end
 if Rails.env.production?
 
   CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'config.yml'))[Rails.env].symbolize_keys!
-  
+
   Rails.application.config.action_controller.asset_host = CONFIG[:host]
   Rails.application.config.action_mailer.asset_host = "http://#{CONFIG[:host]}"
 end
