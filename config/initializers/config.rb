5e00aa9334f7de9217184e02b3ab64c9d5d0aab9
Dastrup Kennet <dastrup-star@Dastrups-Mac.local>
2017-10-18 14:02:25 +0200

    change config.rb

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -6,3 +6,11 @@ if Rails.env.development?
   Rails.application.config.action_controller.asset_host = CONFIG[:host]
   Rails.application.config.action_mailer.asset_host = "http://#{CONFIG[:host]}"
 end
+
+if Rails.env.production?
+
+  CONFIG = YAML.load_file(File.join(Rails.root, 'config', 'config.yml'))[Rails.env].symbolize_keys!
+  
+  Rails.application.config.action_controller.asset_host = CONFIG[:host]
+  Rails.application.config.action_mailer.asset_host = "http://#{CONFIG[:host]}"
+end
