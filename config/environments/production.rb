a0f47f4328b4b155c091fd9bb8f4f4be9d6b6af7
Dastrup Kennet <dastrup-star@Dastrups-Mac.local>
2017-10-18 10:50:38 +0200

    move to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  # Do not load entire app when precompiling assets
+  config.assets.initialize_on_precompile = false
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
@@ -93,4 +96,14 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  module AssetsInitializers
+    class Railtie < Rails::Railtie
+      initializer "assets_initializers.initialize_rails",
+                  :group => :assets do |app|
+        require "#{Rails.root}/config/initializers/config.rb"
+      end
+    end
+  end
+  
 end
